f52e44af4f982f055990e2d7a5768f33988ac395
gaco123 <104175926+gaco123@users.noreply.github.com>
2023-05-03 20:20:09 -0500

    CommandLineArgs set

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS="--ckpt ./models/Stable-diffusion/meinamix_meinaV9.safetensors --disable-safe-unpickle --share --gradio-debug --disable-nan-check"
 
 call webui.bat
